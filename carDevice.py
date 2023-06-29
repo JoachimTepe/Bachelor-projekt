59b7d9238ab2cb049ba757ef13c2f5a362d8b0b8
JosefineBirkstrom <josefinehjordie@gmail.com>
2023-06-29 18:53:14 +0200

    Added time of day to check the delay of the signal

--- carDevice.py
+++ carDevice.py
@@ -1,11 +1,21 @@
 import connection
 import proxmark3
+import datetime
+
+curr_time = datetime.datetime.now().time()
+
+
+print(curr_time.hour, curr_time.minute, curr_time.second)
 
 # Setting up connection before sniffing
 conn = connection.setup()
 
+print("Pre sniffing" + str(curr_time.hour), curr_time.minute, curr_time.second)
+
 # Running the sniffing command on the proxmark3.
 proxmark3.run_sniff_cmd()
 
-# Sending "graph.pm3" to the other device
+print("post sniffing" + str(curr_time.hour), curr_time.minute, curr_time.second)
+
+# Sending the saved pm3 file to the other device
 connection.send_file(conn)
--- keyDevice.py
+++ keyDevice.py
@@ -1,5 +1,12 @@
 import connection
 import proxmark3
+import datetime
+curr_time = datetime.datetime.now().time()
 
+# Connecting and saving pm3 file
 connection.receive_file()
+
+# # Running the simulation command on the proxmark3.
+print("pre simulation" + str(curr_time.hour), curr_time.minute, curr_time.second)
 proxmark3.run_sim_cmd()
+
